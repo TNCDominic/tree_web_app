0931ba0cac0a648fb5a5d84347cfbd05c63a999d
TNCDominic <dominictai4444@gmail.com>
2024-03-25 20:06:43 +0800

    initial_commit

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Can I have your stories</p>
+        </footer>
+    )
+}
+
+export default Footer
