93d595ba77eef81e7e6b54ebd41f9bd24b536cc1
TNCDominic <dominictai4444@gmail.com>
2024-05-07 22:04:04 +0800

    Resolve dependency conflict.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import Homepage from './Homepage.jsx'
 import BlogContent from './Content.jsx';
 import './index.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-
   <BrowserRouter>
     <main>
       <Routes>
@@ -18,7 +17,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </Routes>
     </main>
   </BrowserRouter>
-
-        
-  </React.StrictMode>,
-)
+  </React.StrictMode>, document.getElementById('root'))
